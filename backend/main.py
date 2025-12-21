a34714e013ab1c8e19eaded38b6863fbb20cd8f5
Kirill <KARum2004@yandex.ru>
2025-12-21 23:19:02 +0300

    Add form to upload an audio file

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,20 @@
-# main.py
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, Form, UploadFile, File
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi.templating import Jinja2Templates
 
-diploma = FastAPI()
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
-@diploma.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("upload.html", {"request": request})
+
+
+@app.post("/upload")
+async def upload_file(audio_file: UploadFile = File(...)):
+    return RedirectResponse(url="/success", status_code=303)
+
+
+@app.get("/success")
+async def success(request: Request):
+    return templates.TemplateResponse("success.html", {"request": request})
