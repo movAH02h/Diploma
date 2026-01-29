b1ffb2fc6f4baf6703733c2dff423d1f2098d68f
Kirill <KARum2004@yandex.ru>
2026-01-29 22:09:29 +0300

    Add first version of the logger

--- app/config.py
+++ app/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     APP_NAME: str = "Speech Transcription API"
     LANGUAGE_MODEL: str = "En"
     UPLOAD_FOLDER: str = "audio_files"
+    LOG_LEVEL: "INFO" # LOG, DEBUG, ERROR
     MAX_FILE_SIZE: int = 100 * 1024 * 1024
     ALLOWED_EXTENSIONS: set = {".wav", ".mp3", ".ogg", ".flac"}
 
--- app/logger.py
+++ app/logger.py
@@ -1,10 +1,36 @@
-from logging import Logger
+import logging
 
 class Logger():
-    def __init__:
+    def __init__(self):
         self.logger = None # lazy initialization
+        self.logger_level = logging.INFO 
+
+
+    def change_logger_level(self, level: str) -> None:
+        if level == "DEBUG":
+            self.logger_level = logging.DEBUG
+        elif level == "INFO":
+            self.logger_level = logging.INFO
+        elif level == "ERROR":
+            self.logger_level = logging.ERROR
+
+        self.logger.setLevel(self.logger_level)
+
+    @property
+    def logger(self):
+        if self.logger is None:
+            self.logger = logging.getLogger(__name__)
+        return self.logger
+
+
+    def debug(self, message: str):
+        self.logger.debug(message)
+    
+    
+    def info(self, message: str):
+        self.logger.info(message)
     
 
-    def log_debug():
-        if self.logger is None:
-            self.logger = Logger()
+    def error(self, message: str):
+        self.logger.error(message)
+    
