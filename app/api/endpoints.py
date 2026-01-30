8318bb3b15d9bc7d15c480358c87fe80af31afec
Kirill <KARum2004@yandex.ru>
2026-01-30 16:17:00 +0300

    Add pipeline

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -6,6 +6,15 @@ from app.config import settings
 
 router = APIRouter()
 
+def create_pipeline():
+    process_audio_service = ProcessAudioService()
+    transcription = TranscriptionService(settings.hf_token)
+    diarization = DiarizationService()
+    
+    process_service.set_next(diarization).set_next(transcription)
+    return process_service
+
+
 @router.post("/process_audio")
 async def transcribe_audio(file: UploadFile = File(...)):
     temp_path = f"{settings.UPLOAD_FOLDER}/{file.filename}"
@@ -14,10 +23,10 @@ async def transcribe_audio(file: UploadFile = File(...)):
         buffer.write(content)
     
     try:
-        print("Create TranscriptionService...")
-        service = TranscriptionService(settings.hf_token)
+        print("Create services for pipeline...")
+        pipeline = create_pipeline()
         print("Process audio...")
-        result = service.process(temp_path)
+        result = pipeline.process(data)
 
         os.remove(temp_path)
         return JSONResponse(content=result)
