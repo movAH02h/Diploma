ae8c00ba1aaf414b9fde77ee621534f7ab643ad1
Kirill <KARum2004@yandex.ru>
2026-01-31 19:54:34 +0300

    Add comments to config file

--- app/config.py
+++ app/config.py
@@ -3,14 +3,19 @@ import os
 
 class Settings(BaseSettings):
     APP_NAME: str = "Speech Transcription API"
-    LANGUAGE_MODEL: str = "En"
     UPLOAD_FOLDER: str = "audio_files"
-    LOG_LEVEL: str = "DEBUG" # LOG, DEBUG, ERROR, WARNING, CRITICAL
     MAX_FILE_SIZE: int = 100 * 1024 * 1024
-    MIN_SEGMENT_DURATION: float = 0.5
     ALLOWED_EXTENSIONS: set = {".wav", ".mp3", ".ogg", ".flac"}
 
+    # Diarization params
     HF_TOKEN: str = os.getenv("HF_TOKEN", "")
+    MIN_SEGMENT_DURATION: float = 0.5
+
+    # Transcription params
+    LANGUAGE_MODEL: str = "En"
+    
+    # Logger params
+    LOG_LEVEL: str = "DEBUG" # LOG, DEBUG, ERROR, WARNING, CRITICAL
 
 
 settings = Settings()
